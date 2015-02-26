95368ff7b8de474e4fa2dd6c1c9f4d4d4980f797
Sergio Berlotto <sergio.berlotto@gmail.com>
2015-02-26 14:07:21 -0300

    Acessos ao Cassandra e Elasticsearch

--- desafio.py
+++ desafio.py
@@ -34,21 +34,56 @@ class ElasticBase(object):
     def new_data(self):
         #verifica registos maiores que LAST_ID
         novos = None
+        for dado in data.elasticsearch:
+            res = self.es.search(
+                index=ELASTIC_DB_NAME,
+                doc_type=dado['table_name'],
+                body={
+                    'query': {
+                        'filtered': {
+                            'filter': {
+                                'range': {
+                                    dado['id_field']: {'gt': self.last_id}
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+            novos = res['hits']['hits']
         return novos
 
-    def exist_than_update(self, registro):
+    def exist_than_update(self, tabela, registro):
         #Se existe o registro no elasticsearch, entao
         # atualiza e retorna True, senão retorna False
-        pass
+        reg = es.get(
+            index=ELASTIC_DB_NAME,
+            doc_type=tabela['table_name'],
+            id=registro['_id']
+        )
+        if reg:
+            es.update(
+                index=ELASTIC_DB_NAME,
+                doc_type=tabela['table_name'],
+                id=registro['_id'],
+                body={'doc': registro}
+            )
+            return True
+        else:
+            return False
 
-    def insert(self, registro):
-        pass
+    def insert(self, tabela, registro):
+        self.es.index(
+            index=ELASTIC_DB_NAME,
+            doc_type=tabela['table_name'],
+            body=registro
+        )
 
     def syncronize_new_data(self, registros):
-        for registro in registros:
-            # Retorna false caso não exista
-            if not self.exist_than_update(registro):
-                self.insert(registro)
+        tabela, registro = tabela_registro
+        # Retorna false caso não exista
+        if not self.exist_than_update(tabela, registro):
+            self.insert(tabela, registro)
 
 
 class CassandraBase(object):
@@ -63,27 +98,60 @@ class CassandraBase(object):
         return session
 
     def new_data(self):
+        # Coomo gerador, retorna cada registro e sua tabela
         s = self.session
         for tabela in data.cassandra:
-            print "Verificando %s" % tabela
-            
-        #verifica registros maiores que LAST_ID
-        novos = None  # Aqui consulta no cassandra
-        return novos
+            print "Verificando %s" % tabela['table_name']
+            novos = s.execute(
+                'SELECT * FROM %s where %s > %s' % (
+                    tabela['table_name'],
+                    tabela['id_field'],
+                    self.last_id
+                )
+            )
+            for novo in novos:
+                yield (tabela, novo)
 
-    def exist_than_update(self, registro):
+    def exist_than_update(self, tabela, registro):
         #Se existe o registro no cassandra, entao
         # atualiza e retorna True, senão retorna False
-        pass
+        s = self.session
+        one = s.execute(
+            "SELECT * FROM %s WHERE %s = '%s'" % (
+                tabela['table_name'],
+                tabela['id_field'],
+                registro[tabela['id_field']]
+            )
+        )
+        if one:
+            # Update
+            return True
+        else:
+            return False
 
-    def insert(self, registro):
-        pass
+    def insert(self, tabela, registro={}):
+        if not registro:
+            return
+        # registro é um dicionário de dados
+        SQL = "INSERT INTO %s " % tabela['table_name']
+        fields = []
+        fields_str = ""
+        for key in registro.keys():
+            SQL += "%s, " % key
+            if type(registro[key] == int):
+                fields_str += "%d, "
+            else:
+                fields_str += "%s, "
+            fields.append(registro[key])
+        SQL += ") VALUES ( %s )" % fields_str
 
-    def syncronize_new_data(self, registros):
-        for registro in registros:
-            # Retorna false caso não exista
-            if not self.exist_than_update(registro):
-                self.insert(registro)
+        session.execute(SQL, fields)
+
+    def syncronize_new_data(self, tabela_registro):
+        tabela, registro = tabela_registro
+        # Retorna false caso não exista
+        if not self.exist_than_update(tabela, registro):
+            self.insert(tabela, registro)
 
 
 def daemonize():
@@ -107,12 +175,11 @@ def syncronize():
 
     while True:
 
-        c_new_data = c.new_data()
-        e_new_data = e.new_data()
-        if c_new_data:
-            e.syncronize_new_data(c_new_data)
-        if e_new_data:
-            c.syncronize_new_data(e_new_data)
+        for data in c.new_data():
+            e.syncronize_new_data(data)
+
+        for data in e.new_data():
+            c.syncronize_new_data(data)
 
         sleep(PERIODICIDADE * 60)
 
